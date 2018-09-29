9f523da34ec82bb903f3efc5b3a2cc160d88432f
Prashant Gaurav <prashantgaurav1994@gmail.com>
2018-09-29 12:18:00 +0530

    change
    change

--- api/models/todoModels.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const todoSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, require: true },
-  price: { type: Number, default: 1 }
-});
-module.exports = mongoose.model("Todo", todoSchema);
